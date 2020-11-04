5cd3476129e90d149bbf53f4b28a74250be54d9c
Yichun Tsai <ch543ch543@gmail.com>
2020-11-04 09:57:26 +0800

    first commit

---
+++ src/components/Pagination.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="pagination">
+     <div class="pagination-row">
+            <button class="pagination-botton"> &lt; </button>
+         <span v-for= "(item, index) in new Array(10)" :key="index">
+            <button class="pagination-botton" @click="page(index)">{{ index+1 }}</button>
+         </span>
+            <button class="pagination-botton"> &gt; </button>
+     </div>
+    </div>
+  </template>
+  
+  <script>
+  
+  
+  export default {
+    name: "pagination",
+    data(){
+        return{
+            pageNumber: 1,
+        }
+    },
+    methods:{
+        page(num) {
+            this.pageNumber = num+1
+            console.log(this.pageNumber)
+        }
+    }
+  };
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped lang="scss">
+  .pagination-botton{
+      margin: 3px;
+      padding: 6px;
+      border-radius: 3px;
+      border-style: none;
+      cursor: pointer;
+  }
+  </style>
+  
--- src/main.js
+++ src/main.js
@@ -2,11 +2,41 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import Loading from 'vue-loading-overlay' //component
+import 'vue-loading-overlay/dist/vue-loading.css' //style
 
 Vue.config.productionTip = false;
+Vue.component('Loading', Loading)
+
+Vue.filter('currency',function(value){
+  return value.replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+});
+
+Vue.filter('date',function(value){
+  let date = new Date (value)
+  let month = date.getMonth() + 1;
+  let day = date.getDate();
+  if (month < 10) {
+    month = "0" + month;
+  }
+  if (day < 10) {
+    day = "0" + day;
+  }
+  return month + "/" + day
+});
+
+Vue.filter('day',function(value){
+  let day = new Date (value)
+  return "日一二三四五六".charAt(day.getDay())
+});
 
 new Vue({
   router,
   store,
+  data () {
+    return {
+      info: null
+    }
+  },
   render: h => h(App)
 }).$mount("#app");
